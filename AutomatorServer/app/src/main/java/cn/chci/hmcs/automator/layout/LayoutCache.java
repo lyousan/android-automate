7af0810ca1440f238fa83165ad75c4be18463de8
杨京三 <2762027866@qq.com>
2022-07-26 15:35:54 +0800

    增加布局、节点缓存

---
+++ AutomatorServer/app/src/main/java/cn/chci/hmcs/automator/layout/LayoutCache.java
@@ -0,0 +1,24 @@
+package cn.chci.hmcs.automator.layout;
+
+import org.dom4j.Document;
+
+import java.util.List;
+
+// TODO 界面元素的缓存
+public class LayoutCache {
+    public static Document document;
+    public static NodeInfo root;
+
+    public static void save(String xml) {
+
+    }
+
+    public static String findLocalId(String xpath) {
+        return null;
+    }
+
+    public static List<String> findLocalIds(String xpath) {
+        return null;
+    }
+
+}
